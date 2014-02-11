2125218e91b7ea1a8efd335600cefcaabbbfac6f
Emmanuel Caruyer <caruyer@gmail.com>
2014-02-11 15:35:14 -0500

    Added function to create cortical shell mask.

--- phantomas/geometry/rois.py
+++ phantomas/geometry/rois.py
@@ -17,15 +17,26 @@ def cortical_shell_mask(voxel_size, image_size, phantom_center, phantom_radius,
 
     corner_positions = compute_corner_positions(voxel_size, image_size)
 
+    center_to_corners = corner_positions - phantom_center
+    dst_to_corners = (point_to_corners ** 2).sum(-1)
+
     out_radius = phantom_radius
     in_radius = phantom_radius - shell_thickness
 
     corner_indices = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1],
                       [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
 
-    inside_outer_boundary = np.zeros((dim_x, dim_y, dim_z), dtype=np.bool)
-    outside_inner_boundary = np.zeros((dim_x, dim_y, dim_z), dtype=np.bool)
+    mask_shell = np.zeros((dim_x, dim_y, dim_z), dtype=np.bool)
 
-    
-    
+    for x, y, z in corner_indices:
+        mask_corner_in_out = dst_to_corners[x:dim_x+x, y:dim_y+y, z:dim_z+z] \
+                              <= out_radius ** 2
+        mask_corner_out_in = dst_to_corners[x:dim_x+x, y:dim_y+y, z:dim_z+z] \
+                              >= in_radius ** 2
+        mask_corner = np.logical_and(mask_corner_in_out, mask_corner_out_in)
+
+        np.logical_or(mask_shell, mask_corner, out=mask_shell)
+
+    return mask_shell
+
 
