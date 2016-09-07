97e1de4f904f2ba0acdc62fe63eb24a28ee7444e
Oscar Esteban <code@oscaresteban.es>
2016-09-06 20:13:16 -0700

    [ENH] Use distutils instead of setuptools

--- setup.py
+++ setup.py
@@ -1,36 +1,55 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
-import numpy as np
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
+import sys
 
-extensions = [
-    Extension("phantomas.mr_simul.fast_volume_fraction",
-              ["phantomas/mr_simul/fast_volume_fraction.pyx",
-               "phantomas/mr_simul/c_fast_volume_fraction.c"],
-              include_dirs=[np.get_include(), "/usr/local/include/"],
-              library_dirs=["/usr/lib/"],
-              libraries=["gsl", "gslcblas"]),
-]
+def main():
+    from glob import glob
+    from setuptools import setup, find_packages
+    from setuptools.extension import Extension
 
-setup(name='Phantomas',
-      description='A software phantom generation tool for diffusion MRI.',
-      version='0.1.dev',
-      author='Emmanuel Caruyer',
-      author_email='caruyer@gmail.com',
-      url='http://www.emmanuelcaruyer.com/phantomas/',
-      packages=['phantomas',
-                'phantomas.geometry',
-                'phantomas.mr_simul',
-                'phantomas.utils',
-                'phantomas.visu'],
-      package_data={'phantomas.mr_simul': ["spherical_21_design.txt"]},
-      scripts=[os.path.join('scripts', 'phantomas_struct'),
-               os.path.join('scripts', 'phantomas_dwis'),
-               os.path.join('scripts', 'phantomas_rois'),
-               os.path.join('scripts', 'phantomas_masks'),
-               os.path.join('scripts', 'phantomas_view')],
-      # cmdclass={'build_ext': build_ext},
-      ext_modules=cythonize(extensions),
-      )
+    from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
+    import numpy as np
+
+    REQ_LINKS = []
+    with open('requirements.txt', 'r') as rfile:
+        REQUIREMENTS = [line.strip() for line in rfile.readlines()]
+
+    for i, req in enumerate(REQUIREMENTS):
+        if req.startswith('-e'):
+            REQUIREMENTS[i] = req.split('=')[1]
+            REQ_LINKS.append(req.split()[1])
+
+    if REQUIREMENTS is None:
+        REQUIREMENTS = []
+
+    extensions = [
+        Extension("phantomas.mr_simul.fast_volume_fraction",
+                  ["phantomas/mr_simul/fast_volume_fraction.pyx",
+                   "phantomas/mr_simul/c_fast_volume_fraction.c"],
+                  include_dirs=[np.get_include(), "/usr/local/include/"],
+                  library_dirs=["/usr/lib/"],
+                  libraries=["gsl", "gslcblas"]),
+    ]
+
+    setup(name='Phantomas',
+          description='A software phantom generation tool for diffusion MRI.',
+          version='0.1.dev',
+          author='Emmanuel Caruyer',
+          author_email='caruyer@gmail.com',
+          url='http://www.emmanuelcaruyer.com/phantomas/',
+          packages=find_packages(),
+          package_data={'phantomas.mr_simul': ["spherical_21_design.txt"]},
+          scripts=glob('scripts/*'),
+          # cmdclass={'build_ext': build_ext},
+          ext_modules=cythonize(extensions),
+          )
+
+if __name__ == '__main__':
+    LOCAL_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
+    os.chdir(LOCAL_PATH)
+    sys.path.insert(0, LOCAL_PATH)
+
+    main()
