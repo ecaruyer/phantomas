839532b8e50c64232fd73720dbe0fb9805682492
Oscar Esteban <code@oscaresteban.es>
2016-09-25 11:54:02 -0700

    Improvements to the setuptools installer
    - Added an info.py file where all the distribution variables and
      requirements lists are set.
    - Made independent of requirements.txt

--- phantomas/__init__.py
+++ phantomas/__init__.py
@@ -8,9 +8,22 @@ evaluation of methods in acquisition, signal and image processing, local
 reconstruction and fiber tracking in diffusion MRI. Phantomas is released
 under the terms of the revised BSD license. You should have received a copy
 of the license together with the software, please refer to the file
-``LICENSE``.  
+``LICENSE``.
 
 """
+from __future__ import absolute_import
+from .info import (
+    __version__,
+    __author__,
+    __email__,
+    __maintainer__,
+    __copyright__,
+    __credits__,
+    __license__,
+    __status__,
+    __description__,
+    __longdesc__
+)
 
 __all__ = ['geometry', 'mr_simul', 'utils', 'visu']
-__version__ = '0.1.dev'
+
--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
+PACKAGE_NAME = 'phantomas'
 
 def main():
+    """ Install entry-point """
+    from os import path as op
     from glob import glob
-    from setuptools import setup, find_packages, Extension
+    from inspect import getfile, currentframe
+    from setuptools import setup, find_packages
     from setuptools.extension import Extension
+    from io import open  # pylint: disable=W0622
     import numpy as np
 
-    REQ_LINKS = []
-    with open('requirements.txt', 'r') as rfile:
-        REQUIREMENTS = [line.strip() for line in rfile.readlines()]
+    this_path = op.dirname(op.abspath(getfile(currentframe())))
 
-    for i, req in enumerate(REQUIREMENTS):
-        if req.startswith('-e'):
-            REQUIREMENTS[i] = req.split('=')[1]
-            REQ_LINKS.append(req.split()[1])
+    # Python 3: use a locals dictionary
+    # http://stackoverflow.com/a/1463370/6820620
+    ldict = locals()
 
-    if REQUIREMENTS is None:
-        REQUIREMENTS = []
+    # Get version and release info, which is all stored in phantomas/info.py
+    module_file = op.join(this_path, PACKAGE_NAME, 'info.py')
+    with open(module_file) as infofile:
+        pythoncode = [line for line in infofile.readlines() if not line.strip().startswith('#')]
+        exec('\n'.join(pythoncode), globals(), ldict)
+
 
     extensions = [Extension(
             "phantomas.mr_simul.fast_volume_fraction",
@@ -31,25 +35,33 @@ def main():
             libraries=["gsl", "gslcblas"]),
     ]
 
-    setup(name='phantomas',
-          description='A software phantom generation tool for diffusion MRI.',
-          version='0.1.dev',
-          author='Emmanuel Caruyer',
-          author_email='caruyer@gmail.com',
-          url='http://www.emmanuelcaruyer.com/phantomas/',
-          setup_requires=['setuptools>=18.0', 'numpy', 'cython'],
-          install_requires=REQUIREMENTS,
-          dependency_links=REQ_LINKS,
-          packages=find_packages(),
-          package_data={'phantomas.mr_simul': ["spherical_21_design.txt"]},
-          scripts=glob('scripts/*'),
-          ext_modules=extensions,
+    setup(
+        name=PACKAGE_NAME,
+        version=ldict['__version__'],
+        description=ldict['__description__'],
+        long_description=ldict['__longdesc__'],
+        author=ldict['__author__'],
+        author_email=ldict['__email__'],
+        maintainer=ldict['__maintainer__'],
+        maintainer_email=ldict['__email__'],
+        license=ldict['__license__'],
+        url=ldict['URL'],
+        download_url=ldict['DOWNLOAD_URL'],
+        classifiers=ldict['CLASSIFIERS'],
+        packages=find_packages(exclude=['build', 'doc', 'examples', 'scripts']),
+        package_data={'phantomas.mr_simul': ["spherical_21_design.txt"]},
+        scripts=glob('scripts/*'),
+        ext_modules=extensions,
+        zip_safe=False,
+        # Dependencies handling
+        setup_requires=ldict['SETUP_REQUIRES'],
+        install_requires=ldict['REQUIRES'],
+        dependency_links=ldict['LINKS_REQUIRES'],
+        tests_require=ldict['TESTS_REQUIRES'],
+        extras_require=ldict['EXTRA_REQUIRES'],
     )
 
-if __name__ == '__main__':
-    LOCAL_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
-    os.chdir(LOCAL_PATH)
-    sys.path.insert(0, LOCAL_PATH)
 
+if __name__ == '__main__':
     main()
 
